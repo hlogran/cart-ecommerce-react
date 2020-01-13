aa2daf5dc6df9e29ba232a9f5650723827c09042
Hernan <hernanlogran@hotmail.com>
2020-01-13 20:02:02 -0300

    Create useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import TopMenu from "./components/TopMenu";
+import useFetch from "./hooks/useFetch";
+import { API_URL } from "./utils/constants";
 
 function App() {
+  const result = useFetch(API_URL);
+  console.log(result);
+
   return (
     <div>
       <TopMenu />
