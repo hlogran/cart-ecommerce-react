c7442c0fd1a4c119b18d5bb56fbf03129a18c8e5
Hernan <hernanlogran@hotmail.com>
2020-01-14 17:04:37 -0300

    Add array functions

---
+++ src/utils/arrayFunctions.js
@@ -0,0 +1,17 @@
+export const countDuplicatedItems = (arr, value) => {
+  let count = 0;
+  arr.forEach(x => x === value && count++);
+  return count;
+};
+
+export const removeDuplicatedItems = arr => {
+  return Array.from(new Set(array));
+};
+
+export const removeItem = (arr, item) => {
+  const index = arr.indexOf(item);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+};
