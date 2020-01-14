d04f895f3be445da01ef39ba8fde4cbde1d1f86a
Hernan <hernanlogran@hotmail.com>
2020-01-14 16:12:01 -0300

    Create openCart and closeCart functions

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { ReactComponent as CartEmpty } from "../../assets/svg/cart-empty.svg";
 
 import "./styles.scss";
 
 export default function Cart() {
+  const [cartOpen, setCartOpen] = useState(false);
+
+  const cartWidth = cartOpen ? "400px" : 0;
+
+  const openCart = () => {
+    setCartOpen(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const closeCart = () => {
+    setCartOpen(false);
+    document.body.style.overflow = "scroll";
+  };
+
   return (
     <>
       <Button variant="link" className="cart-button">
-        <CartEmpty />
+        <CartEmpty onClick={openCart} />
       </Button>
-      <div className="cart-content">Here go the products</div>
+      <div className="cart-content" style={{ width: cartWidth }}>
+        Here go the products
+      </div>
     </>
   );
 }
