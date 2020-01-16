32eef500f443e2beb6200f3767d9fbfb49d10cf0
Hernan <hernanlogran@hotmail.com>
2020-01-15 21:00:47 -0300

    Updates

---
+++ precache-manifest.4db478e6969318b0a51686ad247d371c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d541ee709f09f36c0982a08d6dcb7a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4e97e27b6064482370",
+    "url": "/static/css/2.82caf9b5.chunk.css"
+  },
+  {
+    "revision": "840645e31b3cd43a365f",
+    "url": "/static/css/main.fa1d3948.chunk.css"
+  },
+  {
+    "revision": "2b4e97e27b6064482370",
+    "url": "/static/js/2.b20a0d9b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.b20a0d9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "840645e31b3cd43a365f",
+    "url": "/static/js/main.4709e7b4.chunk.js"
+  },
+  {
+    "revision": "78c766b19beb54354049",
+    "url": "/static/js/runtime-main.993be880.js"
+  },
+  {
+    "revision": "44a18073ba6363180ea020efb99eee38",
+    "url": "/static/media/cart-empty.44a18073.svg"
+  },
+  {
+    "revision": "d7ac4f553f3f5bc842323e1ba8bf6d01",
+    "url": "/static/media/cart-full.d7ac4f55.svg"
+  },
+  {
+    "revision": "ebb5848c9a47fd1b781d8d04f94dfd7c",
+    "url": "/static/media/close.ebb5848c.svg"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/static/media/garbage.650cdc57.svg"
+  },
+  {
+    "revision": "c66c92cea4decee3fbcf912cf71cbd10",
+    "url": "/static/media/logo.c66c92ce.svg"
+  }
+]);
